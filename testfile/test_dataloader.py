a9425070e2ec9bb54c26f033f11eb619131cd343
zjc798719285 <798719285@qq.com>
2019-01-29 13:31:52 +0800

    修改dataloader，三个尺度同时输出

--- testfile/test_dataloader.py
+++ testfile/test_dataloader.py
@@ -1,10 +1,11 @@
 from DataLoader import dataset
 from torch.utils.data import DataLoader
-import cv2
+
+import time
 import numpy as np
 import torch
 
-batch_size = 1
+batch_size = 24
 trainlist = '../cfg/trainlist_7.txt'
 label = 'E:\Person_detection\Dataset\Yolov3_labels\labels'
 
@@ -29,26 +30,13 @@ def decodebbox(targets, anchors):
 
 if __name__ =='__main__':
 
- for step, (image, t13, t26, t52, tcls13, tcls26, tcls52, target) in enumerate(dataloader):
-     print(step)
-     Image = image.numpy()[0, ...]*255
-     if Image.shape[1] != 416 or Image.shape[2] !=416:
-         break
-     Image = np.transpose(Image, [1, 2, 0]).astype(np.uint8).copy()
-     anchor13 = np.array([[116, 90], [156, 198], [373, 326]])
-     anchor26 = np.array([[30, 61], [62, 45], [59, 119]])
-     anchor52 = np.array([[10, 13], [16, 30], [33, 23]])
-     boxes = decodebbox(targets=[t13, t26, t52], anchors=[anchor13, anchor26, anchor52])
-
-
-     for ti in target:
-         x, y, w, h = ti[1]*416, ti[2]*416, ti[3]*416, ti[4]*416
-         xmin, ymin, xmax, ymax = int(x - w/2), int(y-h/2), int(x+w/2), int(y+h/2)
-         cv2.rectangle(Image, (xmin, ymin), (xmax, ymax), 2)
-     cv2.imshow('frame', Image)
-     while True:
-         if cv2.waitKey(1) & 0xFF == ord(' '):  # 按q停止
-             break
+ for epoch in range(10):
+    t1 = time.time()
+    for step, (image, tcoord) in enumerate(dataloader):
+        tcoord = tcoord.numpy()
+        print(epoch, step)
+    t2 = time.time()
+    print('epoch time:', (t2 - t1))
 
 
 
